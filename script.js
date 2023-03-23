41c73f9780fa862a060fd1852397845b47ebe5cd
arthurbfrancisco <arthurbfrancisco@gmail.com>
2023-03-24 02:30:03 +0700

    function JS

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 
-let myName = 'Arthur';
-document.write( "Hello, welcome my name is  " + myName)
-
-
-let lastVaca = prompt("What year was the last time you traveled!");
-if (lastVaca <= '2000') {
- document.write (" You're due for some solo travel! ");
-} else {
-    document.write(' Hurry and save so you can plan soon!')
+function inviteUser () {
+    let yourName = prompt("Good day, what is your name?")
+    
 }
 
+let myName = 'Arthur';
+document.write( "Hello, welcome my name is  " + myName + " and this is my blog ")
+
+
+
+
+
+
+function lastVaca() {
+console.log(dateYear);
+    return dateYear;
+   }
+
+let dateYear = prompt("What year was the last time you traveled!");
+if (lastVaca <= '2020') {
+document.write (" You're due for some solo travel! ");
+} else if (lastVaca >= '0' | lastVaca <='9999' ){
+document.write ("Please type correct year!");
+} else {
+document.write(' Hurry and save so you can plan soon!')
+}
+   
+
+
+lastVaca(2021);
+lastVaca(2022);
+lastVaca(2010);
