262e45664f7d0ac00e4928645daa4b4f241a72f9
arthurbfrancisco <arthurbfrancisco@gmail.com>
2023-03-23 08:45:21 +0700

    update with css and jas

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let myName = 'Arthur';
+document.write( "Hello, welcome my name is  " + myName)
+
+
+let bornday = prompt("Type D.O.B Year");
+if (bornday <= '2000') {
+ document.write (" You're old ");
+} else {
+    document.write(' Your what ever')
+}
+
