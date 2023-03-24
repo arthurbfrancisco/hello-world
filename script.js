fe30d087b1cf61b688e14898ce338f54a65dcbd4
arthurbfrancisco <arthurbfrancisco@gmail.com>
2023-03-24 23:37:26 +0700

    update js

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
+document.write( "Hello, welcome my name is Arthur and this is my blog! ");
 
-function inviteUser () {
-    let yourName = prompt("Good day, what is your name?")
+function inviteUser() {
+    let yourName = "";
     
+    while (!yourName || !/^[a-zA-Z]+$/.test(yourName)) {
+      yourName = prompt("Please enter a valid name (letters only)");
+    }
+    
+    if (yourName.toLowerCase() === "mike") {
+      document.getElementById("welcomeMessage").innerHTML = "Glad you made it to my blog, " + yourName + "!";
+    } else {
+      document.getElementById("welcomeMessage").innerHTML = "Welcome to my blog, " + yourName + "!";
+    }
+    
+    return yourName;
+  }
+
+  function myNum() {
+    let yourNum = parseInt(prompt("(How many trips have I taken this year?, between 0-4?)"));
+    if (yourNum < 0 || yourNum > 4 || isNaN(yourNum)) {
+      document.write("Please enter a valid number between 0 and 4.");
+    } else if (yourNum !== 2) {
+      document.write("Guess again!");
+    } else {
+      document.write("Yes, correct..!");
+    }
+    return yourNum;
+  }
+
+function imageCount() {
+    let myImage = prompt("How many pics you want to see?");
+
+    if (isNaN(myImage) || myImage <=0) {
+        alert("Please enter a valid number greater than 0")
+        return;
+    }
+    
+    for (let i = 0; i < myImage; i++ ) {
+        let umage = document.createElement("solo.img");
+ }
+
+ return myImage;
 }
+// Get the user star input
 
-let myName = 'Arthur';
-document.write( "Hello, welcome my name is  " + myName + " and this is my blog ")
-
-
-
-
-
-
-function lastVaca() {
-console.log(dateYear);
-    return dateYear;
-   }
-
-let dateYear = prompt("What year was the last time you traveled!");
-if (lastVaca <= '2020') {
-document.write (" You're due for some solo travel! ");
-} else if (lastVaca >= '0' | lastVaca <='9999' ){
-document.write ("Please type correct year!");
-} else {
-document.write(' Hurry and save so you can plan soon!')
-}
-   
-
-
-lastVaca(2021);
-lastVaca(2022);
-lastVaca(2010);
